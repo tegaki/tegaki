b7fbfc1d1e26bcf4794b8462855811818b974d88
Mathieu Blondel <mathieu@mblondel.org>
2008-07-05 02:34:32 +0900

    A point feature was not written if its value was 0.
    This was due to the fact that 0 is treated as False in Python!

--- libtegaki/tegaki/character.py
+++ libtegaki/tegaki/character.py
@@ -66,7 +66,7 @@ class Point(dict):
         attrs = []
 
         for key in ("x", "y", "pressure", "xtilt", "ytilt", "timestamp"):
-            if self[key]:
+            if self[key] is not None:
                 attrs.append("%s=\"%s\"" % (key, str(self[key])))
 
         return "<point %s />" % " ".join(attrs)
