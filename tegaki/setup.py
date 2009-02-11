b8499171d5f6220dbb563f78f84c27dcafe79943
Mathieu Blondel <mathieu@mblondel.org>
2009-02-11 11:32:55 +0100

    Removed garbage T_T

--- tegaki/setup.py
+++ tegaki/setup.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
-from distutils.sysconfig import get_config_var
-import os
 
 # Please run
-# python setup.py install   
-
-print get_config_var("prefix")
+# python setup.py install  
 
 setup(
     name = 'tegaki',
