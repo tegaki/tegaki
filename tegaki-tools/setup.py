73d87a4553427d9c0e1141805adcf0b8887d010d
Mathieu Blondel <mathieu@mblondel.org>
2009-07-10 15:01:38 +0200

    [tegaki-tools] Added setup.py.

---
+++ tegaki-tools/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import os
+
+def _getversion(path):
+    currdir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(currdir, path)
+    import re
+    regexp = re.compile(r"VERSION = '([^']*)'")
+    f = open(path)
+    buf = f.read()
+    f.close()
+    return regexp.search(buf).group(1)
+
+def getversion():
+    convert = _getversion('src/tegaki-convert')
+    eval_ = _getversion('src/tegaki-eval')
+    build = _getversion('src/tegaki-build')
+    return max(convert, eval_, build)
+
+# Please run
+# python setup.py install   
+
+setup(
+    name = 'tegaki-tools',
+    description = 'A set of command-line tools for Tegaki.',
+    author = 'Mathieu Blondel',
+    author_email = 'mathieu ÂT mblondel DÔT org',
+    url = 'http://tegaki.sourceforge.net',
+    version = getversion(),
+    license='GPL',
+    scripts = ['src/tegaki-convert', 'src/tegaki-build', 'src/tegaki-eval'],
+    packages = ['tegakitools'],
+    package_dir = {'tegakitools':'src/tegakitools'}
+)
