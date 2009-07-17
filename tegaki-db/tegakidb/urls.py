3fb06509fd6099f896853ec3a33521c1f804a952
ian johnson <ianj@ianj-desktop.(none)>
2009-07-17 19:44:47 -0400

    [tegaki-db] minor fix for apache + urls

--- tegaki-db/tegakidb/urls.py
+++ tegaki-db/tegakidb/urls.py
@@ -4,23 +4,23 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^%s$' % settings.BASE_URL, 'tegakidb.news.views.index', name="index"),        #this view could be changed
-    url(r'^%slogin/$' % settings.BASE_URL, 'django.contrib.auth.views.login', {'template_name': 'users/login.html'}, name="login"),
-    url(r'^%slogout/$' % settings.BASE_URL, 'django.contrib.auth.views.logout', {'template_name': 'users/logout.html', 'next_page':'/%s' % settings.BASE_URL}, name="logout"),
+    url(r'^$', 'tegakidb.news.views.index', name="index"),        #this view could be changed
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'users/login.html'}, name="login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'users/logout.html', 'next_page':'/%s' % settings.BASE_URL}, name="logout"),
 
 
-    (r'^%snews/' % settings.BASE_URL, include('tegakidb.news.urls')),
+    (r'^news/', include('tegakidb.news.urls')),
 
-    (r'^%susers/' % settings.BASE_URL, include('tegakidb.users.urls')),
+    (r'^users/', include('tegakidb.users.urls')),
     
-    (r'^%shwdb/' % settings.BASE_URL, include('tegakidb.hwdb.urls')),
+    (r'^hwdb/', include('tegakidb.hwdb.urls')),
 
 
 
 
-    (r'^%sdojango/' % settings.BASE_URL, include('tegakidb.dojango.urls')),
+    (r'^dojango/', include('tegakidb.dojango.urls')),
 
-    (r'^%sadmin/(.*)' % settings.BASE_URL, admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
     #(r'^admin/', include(admin.site.urls)), #this is Django 1.1 version 
 )
 
@@ -29,10 +29,10 @@ urlpatterns = patterns('',
 # should be defined.
 if settings.DEBUG:
     urlpatterns += patterns('',
-    (r'^%sstatic/webcanvas/(?P<path>.*)$' % settings.BASE_URL, 'django.views.static.serve',
+    (r'^static/webcanvas/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.WEBCANVAS_ROOT, 'show_indexes': True}),
 
-    (r'^%sstatic/(?P<path>.*)$' % settings.BASE_URL, 'django.views.static.serve',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     #(r'^dojo/(?P<path>.*)$', 'django.views.static.serve',
     #        {'document_root': settings.DOJO_ROOT, 'show_indexes': True}),
