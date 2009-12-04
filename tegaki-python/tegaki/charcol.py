3ce946ca19fe4b13a737b5e3ec2834cdab30ee6c
Mathieu Blondel <mathieu@mblondel.org>
2009-12-04 21:52:28 +0900

    [tegaki-python] Added get_total_n_strokes to CharacterCollection.

--- tegaki-python/tegaki/charcol.py
+++ tegaki-python/tegaki/charcol.py
@@ -688,14 +688,22 @@ ORDER BY charid LIMIT ? OFFSET ?""", (int(limit), int(offset)))
 
         @rtype: int
         """
-        return self._efo("SELECT count(charid) FROM characters")[0]
+        return self._efo("SELECT COUNT(charid) FROM characters")[0]
+
+    def get_total_n_strokes(self):
+        """
+        Return the total number of strokes in collection.
+
+        @rtype: int
+        """
+        return self._efo("SELECT SUM(n_strokes) FROM characters")[0]
 
     def get_average_n_strokes(self, set_name):
         """
         Return the average number of stroke of the characters in that set.
         """
         i = self._SETIDS[set_name]
-        return self._efo("""SELECT avg(n_strokes) FROM characters
+        return self._efo("""SELECT AVG(n_strokes) FROM characters
 WHERE setid=?""", (i,))[0]
 
     def set_characters(self, set_name, characters):
