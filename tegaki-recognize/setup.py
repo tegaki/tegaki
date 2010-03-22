514a80830e2b96fd556548484268f7af2b6c6b1f
Mathieu Blondel <mathieu@mblondel.org>
2010-03-22 17:36:35 +0900

    [tegaki-recognize] Install desktop and menu files.

--- tegaki-recognize/setup.py
+++ tegaki-recognize/setup.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+from distutils.command.install import install as installbase
 import os
 import sys
+from glob import glob
 
 def getversion():
     currdir = os.path.dirname(os.path.abspath(__file__))
@@ -13,6 +15,52 @@ def getversion():
     buf = f.read()
     f.close()
     return regexp.search(buf).group(1)
+
+def load_file(filename):
+    f = open(filename)
+    ret = f.read()
+    f.close()
+    return ret
+
+def save_file(filename, txt):
+    f = open(filename, "w")
+    f.write(txt)
+    f.close()
+
+class install(installbase):
+    def run(self):
+        installbase.run(self)
+        self._write_file("tegaki-recognize.in", 
+                         os.path.join("share", "menu"),
+                         0644)
+        self._write_file("tegaki-recognize.desktop.in", 
+                         os.path.join("share", "applications"),
+                         0644)
+
+    def _write_file(self, filename, folder, mode):
+        txt = load_file(filename)
+        txt = self._replace_prefix(txt)
+        outdir = os.path.join(self._getprefix(), folder)
+        out = os.path.join(outdir, filename.replace(".in", ""))
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        print "Writing", out
+        save_file(out, txt)
+        os.chmod(out, mode)
+
+    def _replace_prefix(self, txt):
+        return txt.replace("@PREFIX", self._getprefix())       
+
+    def _getprefix(self):
+        try:
+            # >= 2.6
+            if self.prefix_option is None:
+                return "/usr/local"
+            else:
+                return self.prefix_option
+        except AttributeError:
+            # < 2.6
+            return self.prefix
 
 # Please run
 # python setup.py install   
@@ -50,6 +98,8 @@ else:
     # Normally unix-like platforms will use "setup.py install"
     # and install the main script as such
     scripts=[mainscript],
+    data_files=[('share/pixmaps/', glob("data/icons/*.svg"))],
+    cmdclass={'install':install}
      )
 
 
